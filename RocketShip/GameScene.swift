0f518bc7d7e6f2713faafcbad915a0a8cfc125ab
36Boxes <46636974+36Boxes@users.noreply.github.com>
2021-03-08 17:12:38 +0000

    Worked on creating the game area bounds

--- RocketShip/GameScene.swift
+++ RocketShip/GameScene.swift
@@ -10,100 +10,97 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    // Adding the player to the scene
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+    let player = SKSpriteNode(imageNamed: "playerShip")
 
-        self.lastUpdateTime = 0
+    // random number generator
+    
+    
+    
+    
+    let gameArea: CGRect
+    
+    override init(size: CGSize){
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        // to have this work nicely on most devices i could maybe identify the device and then have the correct aspect ratio
+        let maxAspectRatio: CGFloat = 19.5/9
+        let gameAreaWidth = size.height / maxAspectRatio
+        let margin = (size.width - gameAreaWidth) / 2
+        gameArea = CGRect(x: margin, y: 0, width: gameAreaWidth, height: size.height)
+        super.init(size:size)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+    override func didMove(to view: SKView) {
+        
+        
+        // Adding the background to the scene
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.size = CGSize (width: frame.maxX, height: frame.maxY)
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0
+        self.addChild(background)
+                
+        player.setScale(1)
+        
+        // We times the height by 0.2 as we want the ship to start 20% up from the bottom
+        
+        player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
+        player.zPosition = 2
+        self.addChild(player)
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    // Fire Bullet function
+    
+    func BrukItOFF() {
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(1)
+        bullet.position = player.position
+        bullet.zPosition = 1
+        self.addChild(bullet)
+        
+        let fireBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let removeBullet = SKAction.removeFromParent()
+        let ActionsToPerform = SKAction.sequence([fireBullet, removeBullet])
+        bullet.run(ActionsToPerform)
+    }
+    
+    // Function for creating enemies
+    
+    func createOPPS(){
+        
+        
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        BrukItOFF()
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        for touch: AnyObject in touches{
+            
+            let pointOfTouch = touch.location(in: self)
+            let prevoiusTouch = touch.previousLocation(in: self)
+            
+            let difference = pointOfTouch.x - prevoiusTouch.x
+            
+            player.position.x  += difference
+            
+            if player.position.x >= gameArea.maxX - player.size.width/2{
+                player.position.x = gameArea.maxX - player.size.width/2
+            }
+            
+            if player.position.x < gameArea.minX + player.size.width{
+                player.position.x = gameArea.minX + player.size.width/2
+            }
         }
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
 }
